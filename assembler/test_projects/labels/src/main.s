75a2ab15e85b80109146dcff21e7be0c55882fcb
0xABADCAFE <karlchurchill@gmail.com>
2021-07-12 19:26:38 +0100

    Tests.

---
+++ assembler/test_projects/labels/src/main.s
@@ -0,0 +1,22 @@
+
+;  888b     d888  .d8888b.   .d8888b.      d8888  888    d8P
+;  8888b   d8888 d88P  Y88b d88P  Y88b    d8P888  888   d8P
+;  88888b.d88888 888    888 888          d8P 888  888  d8P
+;  888Y88888P888 888        888d888b.   d8P  888  888d88K
+;  888 Y888P 888 888        888P "Y88b d88   888  8888888b
+;  888  Y8P  888 888    888 888    888 8888888888 888  Y88b
+;  888   "   888 Y88b  d88P Y88b  d88P       888  888   Y88b
+;  888       888  "Y8888P"   "Y8888P"        888  888    Y88b
+;
+;   - 64-bit 680x0-inspired Virtual Machine and assembler -
+;
+; My first project - main.s
+
+    @export main
+    @export do_something
+main:
+    bsr do_something
+    rts
+do_something:
+    add.q 32(a0, d1.w * 8), d0
+    rts
