81f3f30c42c2b6e29576fb5673c42ea2018c52d2
0xABADCAFE <karlchurchill@gmail.com>
2021-10-21 20:10:56 +0100

    Fiddling - Added utility to rebuild all assembler projects. - Identified a bug in the fast path optimisation that skipped when source and destination registers are the same. This is due to the source operand having the "same as dest" EA mode instead of a register.

--- assembler/src/parser/source_line/instruction/FastPathOptimiser.php
+++ assembler/src/parser/source_line/instruction/FastPathOptimiser.php
@@ -28,6 +28,7 @@ use ABadCafe\MC64K\Tokeniser;
 use ABadCafe\MC64K\Parser\EffectiveAddress;
 use ABadCafe\MC64K\Utils\Log;
 use ABadCafe\MC64K\Defs\EffectiveAddress\IRegisterDirect;
+use ABadCafe\MC64K\Defs\EffectiveAddress\IOther;
 
 use function \strlen, \ord, \chr, \substr, \reset, \unpack, \pack;
 
@@ -49,10 +50,10 @@ class FastPathOptimiser {
     const DYADIC_FAST_PATH = [
         IDataMove::MOVE_L   => self::FAST_INT_PREFIX,
         IDataMove::MOVE_Q   => self::FAST_INT_PREFIX,
-        ILogic::AND_L       => self::FAST_INT_PREFIX,
-        ILogic::AND_Q       => self::FAST_INT_PREFIX,
-        ILogic::OR_L        => self::FAST_INT_PREFIX,
-        ILogic::OR_Q        => self::FAST_INT_PREFIX,
+        ILogical::AND_L     => self::FAST_INT_PREFIX,
+        ILogical::AND_Q     => self::FAST_INT_PREFIX,
+        ILogical::OR_L      => self::FAST_INT_PREFIX,
+        ILogical::OR_Q      => self::FAST_INT_PREFIX,
         IArithmetic::EXTB_L => self::FAST_INT_PREFIX,
         IArithmetic::EXTB_Q => self::FAST_INT_PREFIX,
         IArithmetic::EXTW_L => self::FAST_INT_PREFIX,
@@ -130,18 +131,31 @@ class FastPathOptimiser {
     public function attempt(int $iOpcode, string $sOperandByteCode): string {
         if (State\Coordinator::get()->getOptions()->isEnabled(Defs\Project\IOptions::OPT_USE_FAST_PATH)) {
             if (
-                isset(self::DYADIC_FAST_PATH[$iOpcode])
-                && 2 == strlen($sOperandByteCode)
-                && isset(self::OPERANDS[ord($sOperandByteCode[0])])
-                && isset(self::OPERANDS[ord($sOperandByteCode[1])])
+                isset(self::DYADIC_FAST_PATH[$iOpcode]) &&
+                2 == strlen($sOperandByteCode)
             ) {
-                // We could just return the code, but throwing as a code fold allows us to log it
-                throw new CodeFoldException(
-                    "Register to register dyadic fast path",
-                    chr(self::DYADIC_FAST_PATH[$iOpcode]) .
-                    chr((ord($sOperandByteCode[0]) & 0x0F) | ((ord($sOperandByteCode[1]) & 0x0F) << 4)) .
-                    chr($iOpcode)
-                );
+                // Extract the operands to check if they are full on register to reister
+                $iDstOperand = ord($sOperandByteCode[0]);
+                $iSrcOperand = ord($sOperandByteCode[1]);
+                if ($iSrcOperand == IOther::SAME_AS_DEST) {
+                    $iSrcOperand = $iDstOperand;
+                }
+
+                if (
+                    isset(self::OPERANDS[$iDstOperand]) &&
+                    isset(self::OPERANDS[$iSrcOperand])
+                ) {
+                    // We could just return the code, but throwing as a code fold allows us to log it
+                    throw new CodeFoldException(
+                        "Register to register dyadic fast path",
+                        chr(self::DYADIC_FAST_PATH[$iOpcode]) .
+                        chr(
+                            ($iDstOperand & 0x0F) |
+                            (($iSrcOperand & 0x0F) << 4)
+                        ) .
+                        chr($iOpcode)
+                    );
+                }
             } else if (isset(self::SPECIAL_CASES[$iOpcode])) {
                 $cHandler = [$this, self::SPECIAL_CASES[$iOpcode]];
                 $cHandler($iOpcode, $sOperandByteCode);
