edb2ad7497908807a7a52aad943a03e6648199ba
0xABADCAFE <karlchurchill@gmail.com>
2021-12-12 22:26:47 +0000

    Bug in dc.s detected

--- assembler/test_projects/vecmath/src/vec2_tests.s
+++ assembler/test_projects/vecmath/src/vec2_tests.s
@@ -8,15 +8,21 @@ vec2f_tests:
     rts
 
 .test_splat:
-    lea     -8(sp), r8
+    lea     -16(sp), r8
     fmove.s #0.5, fp0
     hcf     vec2f_splat
+    lea     .vec2_test_data, r9
+    move.q  (r9), 8(r8)
 
-
+;    move.q  #2,     r0
+;    bsr     verify_array_singles
+;    bsr     report_result
 
     rts
 
+    @align 0, 8
 .vec2_test_data:
+    dc.b    1,2,3,4,5,6,7,8
     dc.s    #0.5, #0.5
     dc.s    #1.0, #2.0
     dc.s    #1.5, #2.5
