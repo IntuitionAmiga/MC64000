268bc9e91669d664a195543fe0fcb89875bd4769
0xABADCAFE <karlchurchill@gmail.com>
2021-12-17 18:39:13 +0000

    Tidying

--- assembler/src/tests/LabelLocationTest.php
+++ assembler/src/tests/LabelLocationTest.php
@@ -19,6 +19,7 @@ namespace ABadCafe\MC64K\Tests;
 use ABadCafe\MC64K\Utils\TestCase;
 use ABadCafe\MC64K\State;
 use ABadCafe\MC64K\IO;
+use ABadCafe\MC64K\Defs;
 
 /**
  * LabelLocationTest
@@ -35,17 +36,74 @@ class LabelLocationTest extends TestCase {
         'global_4' => 11
     ];
 
+    const ADD_LOCAL_TESTS = [
+        '.local_0' => 2,
+        '.local_1' => 3,
+        '.local_2' => 5,
+        '.local_3' => 7,
+        '.local_4' => 11
+    ];
+
     const ADD_GLOBAL_DUPLICATE_TESTS = [
         'global_3' => 8,
         'global_4' => 13
+    ];
+
+    const ADD_LOCAL_DUPLICATE_TESTS = [
+        '.local_3' => 8,
+        '.local_4' => 13
     ];
 
     /**
      * @inheritDoc
      */
     public function run(): void {
+        $this->testLabelLengthValidation();
+        $this->testReservedWordThrows();
         $this->testAddGlobal();
         $this->testAddGlobalDuplicates();
+        $this->testAddLocal();
+        $this->testAddLocalDuplicateSameFileThrows();
+        $this->testAddLocalDuplicateDifferentFile();
+    }
+
+    private function testLabelLengthValidation(): void {
+        echo "\ttesting: label length validation\n";
+        $oMockSource = new IO\SourceString('', __METHOD__);
+        $oLabelLocation = new State\LabelLocation();
+        try {
+            $sTooShort = '';
+            $oLabelLocation->addGlobal($oMockSource, $sTooShort, 0);
+        } catch (\Throwable $oError) {
+            $this->assertInstanceOf(\LengthException::class, $oError);
+        }
+
+        $sMaxLabel = str_repeat('l', Defs\ILabel::MAX_LENGTH);
+        $oLabelLocation->addGlobal($oMockSource, $sMaxLabel, 17);
+        $this->assertSame(17, $oLabelLocation->getPositionForGlobal($sMaxLabel));
+
+        try {
+            $sTooLong = str_repeat('x', Defs\ILabel::MAX_LENGTH + 1);
+            $oLabelLocation->addGlobal($oMockSource, $sTooLong, 0);
+        } catch (\Throwable $oError) {
+            $this->assertInstanceOf(\LengthException::class, $oError);
+        }
+    }
+
+    private function testReservedWordThrows(): void {
+        echo "\ttesting: reserved word throws\n";
+        $oMockSource = new IO\SourceString('', __METHOD__);
+        $oLabelLocation = new State\LabelLocation();
+
+        $aNames = array_keys(Defs\Register\INames::GPR_MAP) + array_keys(Defs\Register\INames::FPR_MAP);
+
+        foreach ($aNames as $sReserved) {
+            try {
+                $oLabelLocation->addGlobal($oMockSource, $sReserved, 0);
+            } catch (\Throwable $oError) {
+                $this->assertInstanceOf(\InvalidArgumentException::class, $oError);
+            }
+        }
     }
 
     private function testAddGlobal(): void {
@@ -55,6 +113,49 @@ class LabelLocationTest extends TestCase {
         foreach (self::ADD_GLOBAL_TESTS as $sLabel => $iPosition) {
             $oLabelLocation->addGlobal($oMockSource, $sLabel, $iPosition);
             $this->assertSame($iPosition, $oLabelLocation->getPositionForGlobal($sLabel));
+        }
+    }
+
+    private function testAddLocal(): void {
+        echo "\ttesting: add local\n";
+        $oMockSource = new IO\SourceString('', __METHOD__);
+        $oLabelLocation = new State\LabelLocation();
+        foreach (self::ADD_LOCAL_TESTS as $sLabel => $iPosition) {
+            $oLabelLocation->addLocal($oMockSource, $sLabel, $iPosition);
+            $this->assertSame($iPosition, $oLabelLocation->getPositionForLocal($oMockSource, $sLabel));
+        }
+    }
+
+    private function testAddLocalDuplicateSameFileThrows(): void {
+        echo "\ttesting: add local duplicate in same file throws\n";
+        $oMockSource = new IO\SourceString('', __METHOD__);
+        $oLabelLocation = new State\LabelLocation();
+        foreach (self::ADD_LOCAL_TESTS as $sLabel => $iPosition) {
+            $oLabelLocation->addLocal($oMockSource, $sLabel, $iPosition);
+            $this->assertSame($iPosition, $oLabelLocation->getPositionForLocal($oMockSource, $sLabel));
+        }
+        foreach (self::ADD_LOCAL_DUPLICATE_TESTS as $sLabel => $iPosition) {
+            try {
+                $oLabelLocation->addLocal($oMockSource, $sLabel, $iPosition);
+            } catch (\Throwable $oError) {
+                $this->assertInstanceOf(\RuntimeException::class, $oError);
+            }
+        }
+    }
+
+    private function testAddLocalDuplicateDifferentFile(): void {
+        echo "\ttesting: add local duplicate in different file\n";
+        $oMockSource1 = new IO\SourceString('', __METHOD__ . '_1');
+        $oLabelLocation = new State\LabelLocation();
+        foreach (self::ADD_LOCAL_TESTS as $sLabel => $iPosition) {
+            $oLabelLocation->addLocal($oMockSource1, $sLabel, $iPosition);
+            $this->assertSame($iPosition, $oLabelLocation->getPositionForLocal($oMockSource1, $sLabel));
+        }
+
+        $oMockSource2 = new IO\SourceString('', __METHOD__ . '_2');
+        foreach (self::ADD_LOCAL_DUPLICATE_TESTS as $sLabel => $iPosition) {
+            $oLabelLocation->addLocal($oMockSource2, $sLabel, $iPosition);
+            $this->assertSame($iPosition, $oLabelLocation->getPositionForLocal($oMockSource2, $sLabel));
         }
     }
 
